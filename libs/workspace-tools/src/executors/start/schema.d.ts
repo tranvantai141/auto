5e51910680fa1d626753745c8db20f307c760e0c
Pham Anh Tuan <phamtuan210394@gmail.com>
2023-08-07 17:24:16 +0700

    Implementing executor for app start

--- libs/workspace-tools/src/executors/start/schema.d.ts
+++ libs/workspace-tools/src/executors/start/schema.d.ts
@@ -1,1 +1,7 @@
-export interface StartExecutorSchema {} // eslint-disable-line
+// options from https://github.com/react-native-community/cli/blob/main/packages/cli-plugin-metro/src/commands/start/index.ts
+
+export interface ReactNativeStartOptions {
+  port: number;
+  resetCache: boolean; // default is false
+  interactive: boolean; // default is true
+}
